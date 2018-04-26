b82cc9468b70fbb51ddd8e7c8558a5fb8c71c409
Ahmed Adel <faozy1234@gmail.com>
2018-04-26 18:55:43 +0200

    instruction memory added

---
+++ instruction_memory.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+/
+
+module InstrMem(
+    input [31:0] Read_Adress,
+    output [31:0] Data_out
+    );
+    
+    reg [31:0] mem [255:0];
+    
+    initial begin
+    	mem[116] = 'hA1455011;
+    	mem[120] = 'h55AC0021;
+    	mem[124] = 'h1D110012;
+    	mem[128] = 'hA01AD025;
+    	mem[132] = 'hB11100AB;
+    end
+
+always @ (*)
+    begin
+     Data_out = memory[Read_Adress];
+    end
+endmodule
+
