96164644765198fba50f65cc3a89f4d77519f4fb
mary252 <34661289+mary252@users.noreply.github.com>
2018-05-02 23:55:17 +0200

    feat;cpu working

--- jdoodle(2).v
+++ jdoodle(2).v
@@ -65,8 +65,9 @@ always @ (in1,in2,control) begin
 			out <= 32'd0;
 		end
 	end
-	
-	default:out<=in1;
+		3'b111: begin
+		out <= in1 ;
+	end
 	
 	endcase
 
@@ -77,203 +78,228 @@ endmodule
 module ControlUnit(
     input [5:0] opcode, func,
     output reg RegDest, Branch, MemRead ,MemtoReg, MemWrite, ALUSrc, RegWrite,
-    output reg [2:0] aluc
+    output reg [2:0] aluc,
+    output reg [1:0] lh
     );
 
-always @ (opcode or func ) begin
+always @ (* ) begin
 
 
 
    // the R-types 
-   
-   if ( opcode == 6'b000000 ) begin 
-    // ADD
-      if (func == 6'b100000 ) begin 
-         RegDest =  1;
-         Branch = 0;
-         MemRead = 0;
-         MemtoReg = 0;
-         MemWrite = 0;
-         ALUSrc = 0;
-         RegWrite =  1;
-         aluc=3'b000;
+   case(opcode)
+   6'b000000: begin 
+    // ADD-done
+    case(func)
+      6'b100000: begin 
+         RegDest <=  1;
+         Branch <= 0;
+         MemRead <= 0;
+         MemtoReg <= 0;
+         MemWrite <= 0;
+         ALUSrc <= 0;
+         RegWrite <=  1;
+         aluc<=3'b000;
+         lh<=2'b00;
       
         end
    
    // SUB
-       if (func == 6'b100010 ) begin
+        6'b100010: begin
        
-         RegDest = 1;
-         Branch = 0;
-         MemRead = 0;
-         MemtoReg = 0;
-         MemWrite = 0;
-         ALUSrc = 0;
-         RegWrite =  1;
-         aluc=3'b001;
+         RegDest <= 1;
+         Branch <= 0;
+         MemRead <= 0;
+         MemtoReg <= 0;
+         MemWrite <= 0;
+         ALUSrc <= 0;
+         RegWrite <=  1;
+         aluc<=3'b001;
+         lh<=2'b00;
        
          end 
    
     //SLL 
     
-      if (func == 6'b000000 ) begin 
+      6'b000000 : begin 
       
-       RegDest = 1;
-         Branch = 0;
-         MemRead = 0;
-         MemtoReg = 0;
-         MemWrite = 0;
-         ALUSrc = 0;
-         RegWrite =  1;
-         aluc=3'b100;
-      
+       RegDest <= 1;
+         Branch <= 0;
+         MemRead <= 0;
+         MemtoReg <= 0;
+         MemWrite <= 0;
+         ALUSrc <= 0;
+         RegWrite <=  1;
+         aluc<=3'b100;
+      lh<=2'b00;
       
          end
       
       
      //SRL 
-      if( func == 6'b000010) begin
+      6'b000010: begin
       
-       RegDest = 1;
-         Branch = 0;
-         MemRead = 0;
-         MemtoReg = 0;
-         MemWrite = 0;
-         ALUSrc = 0;
-         RegWrite =  1;
-         aluc=3'b101;
+       RegDest <= 1;
+         Branch <= 0;
+         MemRead <= 0;
+         MemtoReg <= 0;
+         MemWrite <= 0;
+         ALUSrc <= 0;
+         RegWrite <=  1;
+         aluc<=3'b101;
+         lh<=2'b00;
          end 
         
       //AND
       
-      if(func == 6'b100100) begin
-       RegDest = 1;
-         Branch = 0;
-         MemRead = 0;
-         MemtoReg = 0;
-         MemWrite = 0;
-         ALUSrc = 0;
-         RegWrite =  1;
-         aluc=3'b010;
-      
+      6'b100100: begin
+       RegDest <= 1;
+         Branch <= 0;
+         MemRead <= 0;
+         MemtoReg <= 0;
+         MemWrite <= 0;
+         ALUSrc <= 0;
+         RegWrite <=  1;
+         aluc<=3'b010;
+      lh<=2'b00;
         end
       
       //OR
-      if( func == 6'b100101 ) begin
+      6'b100101 : begin
       
-      
-       RegDest = 1;
-         Branch = 0;
-         MemRead = 0;
-         MemtoReg = 0;
-         MemWrite = 0;
-         ALUSrc = 0;
-         RegWrite =  1;
-         aluc=3'b011;
+      lh<=2'b00;
+       RegDest <= 1;
+         Branch <= 0;
+         MemRead <= 0;
+         MemtoReg <= 0;
+         MemWrite <= 0;
+         ALUSrc <= 0;
+         RegWrite <=  1;
+         aluc<=3'b011;
         end 
       
       
       
       //STL
       
-      if (func == 6'b101010) begin
-      
-       RegDest = 1;
-         Branch = 0;
-         MemRead = 0;
-         MemtoReg = 0;
-         MemWrite = 0;
-         ALUSrc = 0;
-         RegWrite =  1;
-         aluc=3'b110;
+      6'b101010: begin
+      lh<=2'b00;
+       RegDest <= 1;
+         Branch <= 0;
+         MemRead <= 0;
+         MemtoReg <= 0;
+         MemWrite <= 0;
+         ALUSrc <= 0;
+         RegWrite <=  1;
+         aluc<=3'b110;
          end
       
       
+   endcase
    end
 //BEQ
 
-if( opcode == 6'b000100) begin
-   
-         RegDest =  0;
-         Branch = 1;
-         MemRead = 0;
-         MemtoReg = 0;
-         MemWrite = 0;
-         ALUSrc = 0;
-         RegWrite =  0;
+6'b000100: begin
+   lh<=2'b00;
+         RegDest <=  0;
+         Branch <= 1;
+         MemRead <= 0;
+         MemtoReg <= 0;
+         MemWrite <= 0;
+         ALUSrc <= 0;
+         RegWrite <=  0;
+         aluc<=3'b111;
    
    end
 
 
  // ADDi
-   if( opcode == 6'b001000) begin
-   
-         RegDest =  0;
-         Branch = 0;
-         MemRead = 0;
-         MemtoReg = 0;
-         MemWrite = 0;
-         ALUSrc = 1;
-         RegWrite =  1;
-         aluc=3'b000;
+   6'b001000: begin
+   lh<=2'b00;
+         RegDest <=  0;
+         Branch <= 0;
+         MemRead <= 0;
+         MemtoReg <= 0;
+         MemWrite <= 0;
+         ALUSrc <= 1;
+         RegWrite <=  1;
+         aluc<=3'b000;
          
    end
    // LW 
-   if( opcode == 6'b100011) begin
-         RegDest =  0;
-         Branch = 0;
-         MemRead = 1;
-         MemtoReg = 1;
-         MemWrite = 0;
-         ALUSrc = 1;
-         RegWrite =  1;
+   6'b100011: begin
+         RegDest <=  0;
+         Branch <= 0;
+         MemRead <= 1;
+         MemtoReg <= 1;
+         MemWrite <= 0;
+         ALUSrc <= 1;
+         RegWrite <=  1;
+         aluc<=3'b111;
+         lh<=2'b00;
    end 
     //SW 
-   if( opcode == 6'b100011) begin
-         RegDest =  0;
-         Branch = 0;
-         MemRead = 0;
-         MemtoReg = 1;
-         MemWrite = 1;
-         ALUSrc = 1;
-         RegWrite =  0;
+   6'b100011: begin
+         RegDest <=  0;
+         Branch <= 0;
+         MemRead <= 0;
+         MemtoReg <= 1;
+         MemWrite <= 1;
+         ALUSrc <= 1;
+         RegWrite <=  0;
+         aluc<=3'b111;
+         lh<=2'b00;
    end
    // LH
-   if( opcode == 6'b100001) begin
-         RegDest =  0;
-         Branch = 0;
-         MemRead = 1;
-         MemtoReg = 1;
-         MemWrite = 0;
-         ALUSrc = 1;
-         RegWrite =  1;
+   6'b100001: begin
+         RegDest <=  0;
+         Branch <= 0;
+         MemRead <= 1;
+         MemtoReg <= 1;
+         MemWrite <= 0;
+         ALUSrc <= 1;
+         RegWrite <=  1;
+         aluc<=3'b111;
+         lh<=2'b01;
+   end
+   6'b000001: begin
+         RegDest <=  0;
+         Branch <= 0;
+         MemRead <= 1;
+         MemtoReg <= 1;
+         MemWrite <= 0;
+         ALUSrc <= 1;
+         RegWrite <=  1;
+         aluc<=3'b111;
+         lh<=2'b10;
    end
    
-   
    // ANDi
-   if( opcode == 6'b001100) begin
-        RegDest = 1;
-         Branch = 0;
-         MemRead = 0;
-         MemtoReg = 0;
-         MemWrite = 0;
-         ALUSrc = 1;
-         RegWrite =  1;
-         aluc=3'b010;
+   6'b001100: begin
+        RegDest <= 1;
+         Branch <= 0;
+         MemRead <= 0;
+         MemtoReg <= 0;
+         MemWrite <= 0;
+         ALUSrc <= 1;
+         RegWrite <=  1;
+         aluc<=3'b010;
+         lh<=2'b00;
    end
    
    // ori
-   if( opcode == 6'b001101) begin
-         RegDest = 1;
-         Branch = 0;
-         MemRead = 0;
-         MemtoReg = 0;
-         MemWrite = 0;
-         ALUSrc = 1;
-         RegWrite =  1;
-         aluc=3'b011;
+   6'b001101: begin
+         RegDest <= 1;
+         Branch <= 0;
+         MemRead <= 0;
+         MemtoReg <= 0;
+         MemWrite <= 0;
+         ALUSrc <= 1;
+         RegWrite <=  1;
+         aluc<=3'b011;
+         lh<=2'b00;
    end
-	
+endcase	
 
 end
 
@@ -282,12 +308,13 @@ endmodule
 module dataMemory(
 	input [31:0] data_input,address,
 	input MemRead,MemWrite,
-	output reg [31:0] data_Out
+	output reg [31:0] data_Out,
+	input [1:0] lh
 	);
 
 reg [31:0] memory [255:0];
 
-
+reg [31:0] temp;
 
 initial begin
 	memory[0] = 'hA01100AB;
@@ -314,31 +341,49 @@ always @ (data_input or address or MemRead or MemWrite )
 
 		if (MemWrite)
                    begin
-			
+		
 			memory[address] = data_input;
+			
 
                    end
               else
                    begin
-
-                       data_Out = memory[address];
+                        if(lh==2'b01)
+                        begin
+                        temp<=memory[address];
+                        data_Out = {{16{temp[15]}},temp[15:0]};
+                        end
+                        else
+                        begin
+                        if(lh==2'b10)
+                        begin
+                        temp<=memory[address];
+                        data_Out = {{16{temp[15]}},temp[15:0]};
+                        end
+                        else
+                        begin
+                        data_Out = memory[address];
+                        end
+                        end
+                        
                    end 
 	
 	end
-	
+//$display("%b",memory[address]);	
 endmodule
 
 module ID(Signextended, Branch, MemRead ,MemtoReg, MemWrite, ALUSrc,RegWrite,RegDest,aluc,Read_out1,
- Read_out2,addr1,addr2,clk, instruction, write_data,write_address,write_enable);
+ Read_out2,addr1,addr2,clk, instruction, write_data,write_address,write_enable,lh);
     input [31:0] instruction, write_data;
     input clk, write_enable;
     input [4:0] write_address;
-    output Branch, MemRead ,MemtoReg, MemWrite, ALUSrc,RegWrite,RegDest;
-    output [2:0] aluc;
+    output wire Branch, MemRead ,MemtoReg, MemWrite, ALUSrc,RegWrite,RegDest;
+    output  [2:0] aluc;
     output wire [31:0] Read_out1,Read_out2,Signextended;
     output [4:0] addr1,addr2;
+    output wire [1:0] lh;
 SignExtender SignExtender(Signextended,instruction[15:0]);
-ControlUnit ControlUnit(instruction[31:26], instruction[5:0], RegDest, Branch, MemRead ,MemtoReg, MemWrite, ALUSrc, RegWrite,aluc);
+ControlUnit ControlUnit(instruction[31:26], instruction[5:0], RegDest, Branch, MemRead ,MemtoReg, MemWrite, ALUSrc, RegWrite,aluc,lh);
 
 RegisterFile RegFile(Read_out1,Read_out2,clk,write_enable,write_data,instruction[25:21],instruction[20:16],write_address);
 assign addr1 = instruction[20:16];
@@ -351,8 +396,8 @@ module EXE(zeroAluout,Muxout,Aluout,incadded,ALUSrc,RegDest,aluc,addr1,addr2,Rea
 	input [4:0] addr1,addr2;
 	input [31:0] Read_out1,Read_out2,signExtended,incinst;
 	output wire zeroAluout;
-	output [4:0] Muxout;
-	output [31:0] Aluout,incadded;
+	output wire[4:0] Muxout;
+	output wire[31:0] Aluout,incadded;
 
 wire [31:0] signshifted,Aluin2;
 assign signshifted= signExtended<<2;
@@ -459,11 +504,22 @@ module InstrMem(
     reg [31:0] mem [255:0];
     
     initial begin
-    	mem[116] = 'hA1455011;
-    	mem[120] = 'h55AC0021;
-    	mem[124] = 'h1D110012;
-    	mem[128] = 'hA01AD025;
-    	mem[132] = 'hB11100AB;
+        mem[0] = 32'b00110100010000110000000000000011;
+    	mem[4] = 32'b00000000010000010001110000000000;
+    	mem[8] = 32'b10001100100000110000000000000000;
+    	mem[12] = 32'b00000000100000110010000000100010;
+    	mem[20] = 32'b00000000100000110010000000000000;
+    	mem[24] = 32'b00000000100000110010000000000010;
+    	mem[26] = 32'b00000000100000110010000000100100;
+    	mem[28] = 32'b00000000100000110010000000100101;
+    	mem[32] = 32'b00000000100000110010000000101010;
+    	mem[36] = 32'b00010000110001110000000000001011;
+    	mem[40] = 32'b00100000010000110000000000000011;
+    	mem[44] = 32'b10001100100000110000000000000000;
+    	mem[48] = 32'b10000100010000110000000000000000;
+    	mem[52] = 32'b00110000010000110000000000000011;
+    	mem[48] = 32'b00000100010000110000000000000000;
+    	
     end
 
 always @ (*)
@@ -472,15 +528,16 @@ always @ (*)
     end
 endmodule
 
-module MEM(branchRes,dataout,Branch,MemRead,MemWrite,zero,aluresultin,Read_out2);
+module MEM(branchRes,dataout,Branch,MemRead,MemWrite,zero,aluresultin,Read_out2,lh);
 	input Branch,MemRead,MemWrite,zero;
 	input [31:0] aluresultin,Read_out2;
+	input [1:0] lh;
 	output branchRes;
 	output [31:0] dataout;
 
 assign branchRes = Branch &zero;
 
-dataMemory dataMemory(Read_out2,aluresultin,MemRead,MemWrite,dataout);
+dataMemory dataMemory(Read_out2,aluresultin,MemRead,MemWrite,dataout,lh);
 
 endmodule
 
@@ -527,13 +584,13 @@ module PCRegister(out,in,clk);
     input [31:0] in;
     output reg [31:0] out;
    initial begin
-    out<=116;
+    out<=0;
     end
 
     always @ (posedge clk)
     begin
         out <= in;
-        $monitor(" %b %b", in,out);
+       // $monitor(" %b %b", in,out);
     end
     
 endmodule
@@ -553,6 +610,7 @@ initial begin
 	regs[4] = 32'd4;
 	regs[5] = 32'd5;
 	regs[6] = 32'd6;
+	regs[7] = 32'd6;
 	regs[10] = 32'd7;
 end
 always @ (addres1, addres2)
@@ -580,7 +638,7 @@ module SignExtender(out,in);
     output reg [31:0] out;
     always @ (in)
     begin
-        out <= {{16{in[15]}},in[15]};
+        out <= {{16{in[15]}},in};
     end
     
 endmodule
@@ -600,7 +658,7 @@ module cpu();
 	
 reg clk;
 integer i = 0;
-
+wire [1:0] lh;
 wire branchresult,write_enable,Branch, MemRead ,MemtoReg, MemWrite, ALUSrc,RegWrite,RegDest,
 BranchIDOut, MemReadIDOut ,MemtoRegIDOut, MemWriteIDOut, ALUSrcIDOut,RegWriteIDOut,RegDestIDOut,zeroExeout,
 BranchEXEOut, MemReadEXEOut ,MemtoRegEXEOut, MemWriteEXEOut,RegWriteEXEOut,zeroEXEOut,MemtoRegWBOut ,
@@ -625,7 +683,7 @@ IF iF(inst,newAddress,pcout);
 IFIDReg IFIDRegister(instOut, incinstOut,newAddress,inst,clk);
 
 ID ID(Signextended, Branch, MemRead ,MemtoReg, MemWrite, ALUSrc,RegWrite,RegDest,aluc,Read_out1,
- Read_out2,addr1,addr2,clk, instOut, write_data,write_address,RegWriteWBOut);
+ Read_out2,addr1,addr2,clk, instOut, write_data,write_address,RegWriteWBOut,lh);
 IDEXERegister IDEXERegister(BranchIDOut, MemReadIDOut ,MemtoRegIDOut, MemWriteIDOut, ALUSrcIDOut,
 RegWriteIDOut,RegDestIDOut,alucIDOut,Read_out1IDOut, Read_out2IDOut,SignextendedIDOut,incinstIDOut,
 addr1IDOut,addr2IDOut,Branch, MemRead ,MemtoReg, MemWrite,
@@ -637,7 +695,7 @@ EXEMEMRegister EXEMEMRegister(BranchEXEOut, MemReadEXEOut ,MemtoRegEXEOut, MemWr
 zeroEXEOut,write_addressEXEOut,aluresultEXEout,Read_out2EXEOut,incinstEXEOut,clk,BranchIDOut, MemReadIDOut,
 MemtoRegIDOut, MemWriteIDOut,RegWriteIDOut,zeroExeout,Muxout,Aluout,Read_out2IDOut,incInstadded);
 	
-MEM MEM(branchresult,dataout,BranchEXEOut,MemReadEXEOut,MemWriteEXEOut,zeroEXEOut,aluresultEXEout,Read_out2EXEOut);
+MEM MEM(branchresult,dataout,BranchEXEOut,MemReadEXEOut,MemWriteEXEOut,zeroEXEOut,aluresultEXEout,Read_out2EXEOut,lh);
 MEMWBRegister MEMWBRegister(MemtoRegWBOut ,RegWriteWBOut,write_address,aluresultWBout,memdataWBout,clk,
 MemtoRegEXEOut,RegWriteEXEOut,write_addressEXEOut,aluresultEXEout,dataout);
 	
@@ -653,12 +711,12 @@ initial begin
 			$display("%b", pcout);
 			$display("if/id reg instOut incinstOut");
 			$display("%b %b",instOut, incinstOut);
+			//$display("exe zeroExeout,Muxout,Aluout,incInstadded,ALUSrcIDOut,RegDestIDOut,alucIDOut,addr1IDOut,addr2IDOut,Read_out1IDOut,Read_out2IDOut,SignextendedIDOut,incinstIDOut");
+			//$display("%b %b %b %b %b %b %b %b %b %b %b %b %b ",zeroExeout,Muxout,Aluout,incInstadded,ALUSrcIDOut,RegDestIDOut,alucIDOut,addr1IDOut,addr2IDOut,Read_out1IDOut,Read_out2IDOut,SignextendedIDOut,incinstIDOut);
 			$display("id/exe BranchIDOut MemReadIDOut MemtoRegIDOut MemWriteIDOut ALUSrcIDOut RegWriteIDOut RegDestIDOut alucIDOut Read_out1IDOut  Read_out2IDOut SignextendedIDOut incinstIDOut addr1IDOut addr2IDOut");
 			$display("%b %b %b %b %b %b %b %b %b %b %b %b %b %b",BranchIDOut, MemReadIDOut ,MemtoRegIDOut, MemWriteIDOut, ALUSrcIDOut,RegWriteIDOut,RegDestIDOut,alucIDOut,Read_out1IDOut, Read_out2IDOut,SignextendedIDOut,incinstIDOut,addr1IDOut,addr2IDOut);
-			$display("exe/exe BranchEXEOut, MemReadEXEOut ,MemtoRegEXEOut, MemWriteEXEOut, RegWriteEXEOut,zeroEXEOut,write_addressEXEOut,aluresultEXEout,Read_out2EXEOut,incinstEXEOut");
-			$display("%b %b %b %b %b",MemtoRegWBOut ,RegWriteWBOut,write_address,aluresultWBout,memdataWBout);
-			$display("%b %b %b %b %b %b %b %b %b %b",BranchEXEOut, MemReadEXEOut ,MemtoRegEXEOut, MemWriteEXEOut,
-			RegWriteEXEOut,zeroEXEOut,write_addressEXEOut,aluresultEXEout,Read_out2EXEOut,incinstEXEOut);
+			$display("exe/mem BranchEXEOut, MemReadEXEOut ,MemtoRegEXEOut, MemWriteEXEOut, RegWriteEXEOut,zeroEXEOut,write_addressEXEOut,aluresultEXEout,Read_out2EXEOut,incinstEXEOut");
+			$display("%b %b %b %b %b %b %b %b %b %b",BranchEXEOut, MemReadEXEOut ,MemtoRegEXEOut, MemWriteEXEOut,RegWriteEXEOut,zeroEXEOut,write_addressEXEOut,aluresultEXEout,Read_out2EXEOut,incinstEXEOut);
 			$display("mwm/wb MemtoRegWBOut ,RegWriteWBOut,write_address,aluresultWBout,memdataWBout");
 			$display("%b %b %b %b %b",MemtoRegWBOut ,RegWriteWBOut,write_address,aluresultWBout,memdataWBout);
 		end
